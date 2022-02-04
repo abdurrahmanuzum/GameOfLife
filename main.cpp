62794ba0da0d3eb90d79ca5010e0cf266648509e
Abdurrahman Uzum <uzum.abdurrahman@outlook.com>
2022-02-05 02:40:43 +0300

    Minimal working graphical setup

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@ SDL_Renderer* renderer = nullptr;
 const int WINDOW_WIDTH = 700;
 const int WINDOW_HEIGHT = 700;
 
-const int POPULATION = 20;
+const int POPULATION = 10;
 
 
-void update( unsigned char** cells, int size )
+void update_cells( unsigned char** cells, int size )
 {
+	//TODO: Optimise for partial updating, don't iterate the regions that are guaranteed to remain unchanged for the next clock
+
 	unsigned char* prev_col = nullptr;
 	unsigned char* curr_col = nullptr;
 	unsigned char* next_col = nullptr;
@@ -23,8 +25,6 @@ void update( unsigned char** cells, int size )
 	memcpy( prev_col, cells[0], size+2 );
 	memcpy( curr_col, cells[1], size+2 );
 	memcpy( next_col, cells[2], size+2 );
-	
-
 	
 	for ( int i = 1; i < size+1; i++ )
 	{
@@ -44,7 +44,7 @@ void update( unsigned char** cells, int size )
 			}
 		}
 
-		if ( i+2 < size+2 )
+		if ( i+2 < size+2 ) //Should be a way around this...
 		{
 			memcpy( prev_col, curr_col, size+2 );
 			memcpy( curr_col, next_col, size+2 );
@@ -67,16 +67,34 @@ void print_to_console( unsigned char** cells, int size )
 	}
 }
 
-void print_to_window( unsigned char** cells, int size_x, int size_y )
+int print_to_window( unsigned char** cells, int size, int x_start_index, int x_end_index, int y_start_index, int y_end_index, SDL_Rect unit_rect, int sub_window_width = WINDOW_WIDTH, int sub_window_height = WINDOW_HEIGHT )
 {
-	for ( int j = 0; j < size_y; j++ )
+	if ( x_start_index < 0 || x_end_index > size || y_start_index < 0 || y_end_index > size )
 	{
-		for ( int i = 0; i < size_x; i++ )
+		printf( "On function print_to_window:\n" );
+		printf( "Invalid indecies detected, aborting printing...\n" );
+		return -1;
+	}
+
+	SDL_SetRenderDrawColor( renderer, 0xff, 0xff, 0xff, 0xff );
+
+	for ( int j = y_start_index; j < y_end_index; j++ )
+	{
+		for ( int i = x_start_index+1; i < x_end_index+1; i++ )
 		{
-			printf( "%d ", cells[i+1][j+1] );
+			unit_rect.x = i * unit_rect.w;
+			unit_rect.y = j * unit_rect.h;
+
+			if ( cells[i+1][j+1] == 1 )
+			{
+				SDL_RenderFillRect( renderer, &unit_rect );
+			}
+			else
+			{
+				SDL_RenderDrawRect( renderer, &unit_rect );
+			}
 			
 		}
-		printf("\n");
 	}
 }
 
@@ -115,15 +133,13 @@ int main ( int argc, char** argv )
 	cells[5][5] = 1;
 	cells[5][6] = 1;
 	cells[5][7] = 1;
-	
-
-	printf( "Initial condition map is as follows:\n\n" );
-	print_to_console( cells, POPULATION+2 );
-	system("pause");
 
 
 	bool quit = false;
 	SDL_Event event;
+
+	SDL_SetRenderDrawColor( renderer, 0x00, 0x00, 0x00, 0xff );
+	SDL_RenderPresent( renderer );
 
 	while ( !quit )
 	{
@@ -134,20 +150,28 @@ int main ( int argc, char** argv )
 				quit = true;
 			}
 		}
+		SDL_SetRenderDrawColor( renderer, 0x00, 0x00, 0x00, 0xff );
+		SDL_RenderClear( renderer );
 
-		for ( int i = 0; i <= 3; i++ )
-		{
-			//printf("\x1B[1;1H\x1B[2J");
+		update_cells( cells, POPULATION );
+		printf("Updated.\n");
+		print_to_window( cells, POPULATION, 0, 10, 0, 10, unit_rect );
+		printf("Printed.\n");
+		SDL_RenderPresent( renderer );
 
-			printf("---------------------------------------------------------------------------------------\n");
 
-			update( cells, POPULATION );
-			print_to_console( cells, POPULATION+2 );			
+		SDL_Delay(1000);
 
-			SDL_Delay(1000);
-		}
-		break;
+
+
 	}
+
+
+
+
+
+
+
 
 
 	for ( int i = 0; i < (POPULATION+2); i++ )
